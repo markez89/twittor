866bfc4674a839dbc9f922136f7df07e73e5fde7
unknown <a.marquez.m.89@gmail.com>
2022-08-03 12:58:32 +0200

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,16 @@
+
+
+//Guardar el cache
+const actualizaCacheDinamico = (dinamicCache, req, res) => {
+
+    if(res.ok){
+
+        return caches.open(dinamicCache).then(cache => {
+            cache.put(req, res.clone());
+
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+}
