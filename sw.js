6ef9f07cc568d495f0fca16f99c7b3f879d97f78
unknown <a.marquez.m.89@gmail.com>
2022-08-03 13:26:58 +0200

    Tercer commit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dinamic-v1';
+const DYNAMIC_CACHE = 'dinamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -40,6 +40,10 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.foreEach(key => {
             if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
                 return caches.delete(key);
             }
         })
